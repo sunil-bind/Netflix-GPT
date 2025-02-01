bb25865a8eb04a31b34cf8dc79dc779545ace11f
sunil-bind <sunil329021@gmail.com>
2025-02-01 15:53:45 +0530

    added form validation

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,10 +1,20 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
+import { checkValidData } from "../utilits/validate";
 
 const Login = () => {
   const [isSignInForm, SetSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
   const handleForm = () => {
     SetSignInForm(!isSignInForm);
+  };
+
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const onFormSubmit = () => {
+    const errorMessage = checkValidData(email.current.value,password.current.value)
+    setErrorMessage(errorMessage);
   };
   return (
     <div>
@@ -14,25 +24,48 @@ const Login = () => {
         src="https://assets.nflxext.com/ffe/siteui/vlv3/fb5cb900-0cb6-4728-beb5-579b9af98fdd/web/IN-en-20250127-TRIFECTA-perspective_cf66f5a3-d894-4185-9106-5f45502fc387_large.jpg"
         alt="background"
       />
-      <form className="d-flex flex-column gap-3 p-4 justify-content-center w-25 position-absolute top-50 start-50 translate-middle rounded shadow bg-dark bg-opacity-75"
-      style={{ minHeight: "300px" }}>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+        className="d-flex flex-column gap-3 p-4 justify-content-center w-25 position-absolute top-50 start-50 translate-middle rounded shadow bg-dark bg-opacity-75"
+        style={{ minHeight: "300px" }}
+      >
         <p className="text-light fw-bold p-2 m-2 fs-4">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </p>
         {!isSignInForm && (
           <input type="email" placeholder="email" className="p-2 m-2 rounded" />
         )}
-        <input type="email" placeholder="email" className="p-2 m-2 rounded" />
         <input
+          ref={email}
+          type="email"
+          placeholder="email"
+          className="p-2 m-2 rounded"
+        />
+        <input
+          ref={password}
           type="password"
           placeholder="password"
           className="p-2 m-2 rounded"
         />
-        <button type="button" className="btn btn-danger p-2 m-2">
+        <p className="text-light fw-bold p-2 m-2 fs-6">
+            {errorMessage}
+        </p>
+        <button
+          onClick={onFormSubmit}
+          type="button"
+          className="btn btn-danger p-2 m-2"
+        >
           Sign In
         </button>
-        <p className="text-light fw-bold p-2 m-4 cursor-pointer" onClick={handleForm}>
-          {isSignInForm?"New to Netflix? Sign UP":"Already Registered? Sign In"}
+        <p
+          className="text-light fw-bold p-2 m-4 cursor-pointer"
+          onClick={handleForm}
+        >
+          {isSignInForm
+            ? "New to Netflix? Sign UP"
+            : "Already Registered? Sign In"}
         </p>
       </form>
     </div>
